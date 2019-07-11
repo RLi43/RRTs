1b8908bf3e2e483752ffb7ad71a490323ea8b704
LiJF <jf-li16@mails.tsinghua.edu.cn>
2019-07-11 20:34:14 +0800

    RRT connect

--- rrts.py
+++ rrts.py
@@ -18,16 +18,17 @@ import math
 import copy
 import matplotlib.pyplot as plt
 import time
+import platform
 
 show_animation = True
 
 class RRT:
-    def __init__(self,_map=None,method="RRT",maxIter=500):
+    def __init__(self,_map=None,method="RRT-Connect",maxIter=500):
         if _map == None:
             self.map = Map()
         else: self.map = _map
         self.method = method
-        self.nodes = [Node(np.array(self.map.xinit))]
+        self.nodes = []
         self.edges = None
         self.dimension = self.map.dimension
         self.prob = 0.1
@@ -43,8 +44,12 @@ class RRT:
         start_time = time.time()
         if self.method == "RRT":
             ret = self.rrtSearch()
+        elif self.method == "RRT-Connect":
+            self.prob = 0
+            ret = self.rrtConnectSearch()
         else:
-            pass
+            print("Unsupported Method, please choose one of:")
+            print("RRT, RRT-Connect")
         end_time = time.time()
         if not ret:
             print("Solve Failed")
@@ -62,12 +67,11 @@ class RRT:
 
         if show_animation:
             self.drawGraph()
+            self.drawPath()
         return ret
 
-    def drawPath(self):
-        return
-
     def rrtSearch(self):
+        self.nodes.append(Node(np.array(self.map.xinit)))
         ngoal = Node(np.array(self.map.xgoal))
         for i in range(self.maxIter):
             xrand = self.SampleRandomFreeFast()
@@ -83,7 +87,85 @@ class RRT:
                     return True
             if show_animation:
                 self.drawGraph(rnd=xrand,new=nnew)
+                plt.pause(0.0001)
         return False
+
+    def rrtConnectSearch(self):
+        ngoal = Node(np.array(self.map.xgoal))
+        ninit = Node(np.array(self.map.xinit))
+        treeI = [ninit]
+        treeG = [ngoal]
+        tree1 = treeI # the less points one
+        tree2 = treeG
+        for i in range(self.maxIter):
+            xrand = self.SampleRandomFreeFast()
+            nnearest = self.Nearest(xrand,nodes=tree1)
+            nnew = self.Extend(nnearest,xrand)
+            if nnew!=None:
+                tree1.append(nnew)
+                ncon = self.theOneNearby(nnew,tree2)
+                if ncon:
+                    self.nodes = tree1+tree2
+                    nLast = nnew2
+                    while ncon:
+                        nNext = ncon.parent
+                        self.nodes.append(ncon)
+                        ncon.parent = nLast
+                        nLast = ncon
+                        ncon = nNext
+                    print("iter ",i," find!")
+                    return True
+            
+                # another tree
+                # directly forward to nnew
+                nnearest = self.Nearest(nnew.x,nodes=tree2)
+                nnew2 = self.Extend(nnearest,nnew.x)
+                while nnew2:
+                    tree2.append(nnew2)
+                    ncon = self.theOneNearby(nnew2,tree1)
+                    if ncon:
+                        self.nodes = tree1+tree2
+                        nLast = nnew2
+                        while ncon:
+                            nNext = ncon.parent
+                            self.nodes.append(ncon)
+                            ncon.parent = nLast
+                            nLast = ncon
+                            ncon = nNext
+                        print("iter ",i," find!")
+                        return True
+                    nnearest = nnew2
+                    nnew2 = self.Extend(nnearest,nnew.x)
+                    
+                    if show_animation:
+                        self.drawGraph(rnd=xrand,new=nnew,drawnodes=False)
+                        self.drawNodes(treeI,'g')
+                        self.drawNodes(treeG,'b')
+                        plt.pause(0.0001)
+
+                # check the size,
+                # let the less one to random spare
+                if(len(tree1)>len(tree2)):
+                    temptree = tree1
+                    tree1 = tree2
+                    tree2 = temptree
+
+            if show_animation:
+                self.drawGraph(rnd=xrand,new=nnew,drawnodes=False)
+                self.drawNodes(treeI,'g')
+                self.drawNodes(treeG,'b')
+                plt.pause(0.0001)
+            
+
+
+        return False
+
+    def theOneNearby(self,node,tree):
+        for n in tree:
+            if Node.distancenn(node,n)<self.stepSize:
+                return n
+        return None
+            
 
     def _CollisionPoint(self,x): 
         obs = self.map.obstacles
@@ -92,17 +174,23 @@ class RRT:
                 return True
         return False
     def _CollisionLine(self,x1,x2):
-        nums = int(Node.distancexx(x1,x2)/self.DISCRETE)
+        dis = Node.distancexx(x1,x2)
+        if dis<self.DISCRETE:
+            return False
+        nums = int(dis/self.DISCRETE)
+        direction = (np.array(x1)-np.array(x2))/Node.distancexx(x1,x2)
         for i in range(nums+1):
-            x = np.add(x1 , [i*self.DISCRETE,]*self.dimension)
+            x = np.add(x1 , i*self.DISCRETE*direction)
             if self._CollisionPoint(x): return True
         if self._CollisionPoint(x2): return True
         return False
 
-    def Nearest(self,xto):
+    def Nearest(self,xto,nodes=None):
+        if nodes == None:
+            nodes = self.nodes
         dis = float('inf')
         nnearest = None
-        for node in self.nodes:
+        for node in nodes:
             curDis = Node.distancenx(node,xto)
             if curDis < dis:
                 dis = curDis
@@ -147,29 +235,46 @@ class RRT:
             ret.append(random.random()*self.map.randLength[i]+self.map.randBias[i])
         return ret
 
-    def drawGraph(self, xCenter=None, cBest=None, cMin=None, etheta=None, rnd=None, new=None):
+
+    def drawGraph(self, xCenter=None, cBest=None, cMin=None, etheta=None, rnd=None, new=None,drawnodes=True):
         if self.dimension!=2:
             return
         plt.clf()
+        sysstr = platform.system()
+        if(sysstr =="Windows"):
+            scale = 18
+        elif(sysstr == "Linux"):
+            scale = 24
+        else: scale = 24
         for (ox, oy, size) in self.map.obstacles:
-            plt.plot(ox, oy, "ok", ms=24 * size)
+            plt.plot(ox, oy, "ok", ms=scale * size)
         if rnd is not None:
             plt.plot(rnd[0], rnd[1], "^k")
         if new is not None:
             plt.plot(new.x[0], new.x[1], "og")
-
-        for node in self.nodes:
-            if node.parent is not None:
-                plt.plot([node.x[0], node.parent.x[0]], 
-                [node.x[1], node.parent.x[1]], "-g")
-
+        
+        if drawnodes:
+            self.drawNodes()
 
         plt.plot(self.map.xinit[0], self.map.xinit[1], "xr")
         plt.plot(self.map.xgoal[0], self.map.xgoal[1], "xr")
         plt.axis([self.map.xinit[0]+self.map.randBias[0],self.map.xinit[0]+self.map.randBias[0]+self.map.randLength[0],
         self.map.xinit[1]+self.map.randBias[1],self.map.xinit[1]+self.map.randBias[1]+self.map.randLength[1]])
         plt.grid(True)
-        plt.pause(0.0001)
+
+    def drawNodes(self,nodes=None,color='g'):
+        if nodes==None:
+            nodes=self.nodes
+        for node in nodes:
+            if node.parent is not None:
+                plt.plot([node.x[0], node.parent.x[0]], 
+                [node.x[1], node.parent.x[1]], '-'+color)
+
+
+
+    def drawPath(self):
+        plt.plot([x for (x, y) in self.path], [y for (x, y) in self.path], '-r')        
+        return
 
 
 
@@ -182,7 +287,7 @@ class Node:
 
     @staticmethod
     def distancenn(n1,n2):
-        return lng.norm(n1.x-n2.x)
+        return lng.norm(np.array(n1.x)-np.array(n2.x))
     @staticmethod
     def distancenx(n,x):
         return lng.norm(n.x-np.array(x))
@@ -202,7 +307,7 @@ class Tree:
 
 
 class Map:
-    def __init__(self,dim=2,obs_num=10,obs_size_max=2,xinit=[0,0],xgoal=[23,23],randLength=[29,29],randBias=[-3,-3]):
+    def __init__(self,dim=2,obs_num=50,obs_size_max=2.5,xinit=[0,0],xgoal=[23,23],randLength=[29,29],randBias=[-3,-3]):
         self.dimension = dim
         self.xinit = xinit
         self.xgoal = xgoal
@@ -223,16 +328,17 @@ def main():
     # create map
     amap = Map()
     
-    rrt = RRT(_map=amap,method="RRT")
+    rrt = RRT(_map=amap)
+    rrt.drawGraph()
+    plt.pause(0.01)
+    input("any key to start")
     rrt.Search()
     print("Finished")
     
     # Plot path
     if  rrt.dimension==2 and show_animation:
         rrt.drawGraph()
-        plt.plot([x for [x, y] in rrt.path], [y for [x, y] in rrt.path], '-r')
-        plt.grid(True)
-        plt.pause(0.01)
+        rrt.drawPath()
         plt.show()
 
 
